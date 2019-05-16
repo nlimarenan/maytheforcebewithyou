0285ec5a2809b61f993f730e950c88500d1570bc
Renan Lima <nlimarenan@gmail.com>
2019-05-16 07:19:04 -0300

    planetentity added

---
+++ src/main/java/com/game/entity/PlanetEntity.java
@@ -0,0 +1,45 @@
+package com.game.entity;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "PLANET")
+public class PlanetEntity {
+	private String id;
+	private String nome;
+	private String clima;
+	private String terreno;
+	
+	@DynamoDBAttribute
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	@DynamoDBAttribute
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@DynamoDBAttribute
+	public String getClima() {
+		return clima;
+	}
+	public void setClima(String clima) {
+		this.clima = clima;
+	}
+	
+	@DynamoDBAttribute
+	public String getTerreno() {
+		return terreno;
+	}
+	public void setTerreno(String terreno) {
+		this.terreno = terreno;
+	}
+}
